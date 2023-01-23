546fb731601cb98632ce1da53c20700c0a245271
Hiroshi Mochizuki <mochizuki.hiroshi@deerworks.tech>
2023-01-24 06:56:35 +0900

    updated redraw

--- main.py
+++ main.py
@@ -4,24 +4,26 @@ data = [[1,2],[3,4]]
 
 class App_(wx.App):
   def OnInit(self):
-    self.Frame_ = Frame_(None, id=-1)
+    self.Frame_ = Frame_(None)
     self.SetTopWindow(self.Frame_)
     self.Frame_.Show(True)
-    self.Frame_.Timer_.Start(milliseconds=50)
     return True
 
 class Frame_(wx.Frame):
-  def __init__(self, *args, **kw):
-    super().__init__(*args, **kw)
-    self.List_ = List_(self, id=-1, style=wx.LC_REPORT|wx.LC_VIRTUAL)
-    self.Timer_ = Timer_(self, id=-1)
-  
-  def re(self):
-    self.List_.SetItemCount(len(data))
+  def __init__(self, parent):
+    super().__init__(parent, id=-1)
+    self.List_ = List_(self)
+    self.timer = Timer_(self)
+    self.Bind(wx.EVT_TIMER, self.redraw, self.timer)
+    self.timer.Start(20)
+
+  def redraw(self, event):
+    data[0][0] = data[0][0] + 1
+    wx.ListCtrl.Refresh(self.List_)
 
 class List_(wx.ListCtrl):
-  def __init__(self, *args, **kw):
-    super().__init__(*args, **kw)
+  def __init__(self, parent):
+    super().__init__(parent, id=-1, style=wx.LC_REPORT|wx.LC_VIRTUAL)
     self.items = data
     self.SetBackgroundColour("#c8c8c8")
     self.InsertColumn(0, 'time')
@@ -31,13 +33,10 @@ class List_(wx.ListCtrl):
   def OnGetItemText(self, item, column):
     items = (self.items[item][column])
     return str(items)
-  
+
 class Timer_(wx.Timer):
-  def __init__(self, *args, **kw):
-    super().__init__(*args, **kw)
-  
-  def Notify(self):
-    data[0][0] = data[0][0] + 1
+  def __init__(self, parent):
+    super().__init__(parent, id=-1)    
 
 def main():
   app = App_(False)
